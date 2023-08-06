52b9763e30f180cc0ed7663e3852487a2e418640
lee <lee@naver.com>
2023-08-07 08:12:00 +0900

    day04

---
+++ js01/js/day01.js
@@ -0,0 +1,63 @@
+console.log('hello');
+/*
+  1. 변수 선언
+   let, var
+
+   상수: const   es6
+*/
+ const irum='hong gil dong';
+ console.log('name',irum);
+// irum='park!!!';
+
+//  console.log('new name', irum);
+
+
+var a;
+console.log('a', a);
+a=10;
+console.log('a',a);
+// a=20;
+// console.log('new-a', a);
+console.log('-------------');
+
+// var b;
+// console.log('b', b);
+
+console.log('==================');
+
+// su=10;
+// console.log('su', su);
+// let su;
+/*
+
+
+
+*/
+let a1=10;
+let b='aaa';
+let c=10.2;
+let d=10==10;
+let f=null;
+let e;
+
+console.log(typeof a1,'a1');
+console.log(typeof b,'b');
+console.log(typeof c,'c');
+console.log(typeof d,'d');
+console.log(typeof e,'e');
+console.log(typeof f,'f');
+console.log(10/0,'10/0');
+console.log(-10/0,'-10/0');
+console.log('aa'/10,'aa/10');
+
+
+let t1=[];  //배열
+let t2={};   //객체
+let t3=function(){
+     console.log('test');
+}; //함수
+console.log(typeof t1);
+console.log(typeof t2);
+console.log(typeof t3);
+
+
