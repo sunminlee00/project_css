3ee3c197aaf9375ff693b1f567a0d36105e23f87
lee <lee@naver.com>
2023-08-07 08:57:22 +0900

    data

---
+++ js02/js/day02.js
@@ -0,0 +1,80 @@
+"use strict"
+/*
+   let  
+   const
+   var
+
+*/
+
+// var a=undefined;
+/*
+let a;
+console.log('a', a);
+*/
+//let, const:  block scope 
+
+// let a=30;
+// {
+//   let a=10;
+//   console.log('block-a', a); //a:10
+// }
+
+// console.log('global-a', a); //a :30
+
+// var su=100;
+// {
+//     var su=10;
+//     console.log('blockd-su',su);
+// }
+
+// console.log('global-su', su);
+
+// console.log('-------------');
+
+// for(var i=1; i<3; i++){
+//     console.log(i);
+// }
+// console.log('i...', i);
+
+//  console.log('------------');
+
+//  let tf='';
+
+//  if(tf)
+//    console.log('true');
+//  else 
+//    console.log('false');
+
+// let arr=[10,20,30];
+
+// let fn=function(){
+//     console.log('function!!!');
+// }
+// fn();
+// console.log('-----');
+// console.log(arr.length);
+
+// for(let i=-0; i<arr.length; i++)
+//     console.log(arr[i]);
+
+// console.log('-------');
+// console.log(typeof arr);
+// console.log(typeof(arr));
+// console.log(typeof fn);
+// console.log(typeof(fn));
+
+// let person={};
+
+// console.log(typeof person);
+// console.log('----------');
+// console.log(Array.isArray(arr));
+// console.log(Array.isArray(person));
+
+let a='10';
+let b=20;
+let c=a+b;
+console.log(typeof c, c);
+console.log(typeof(10/2), 10/2);
+console.log(typeof('10'/2), '10'/2);
+console.log(typeof('a'/2), 'a'/2);
+
