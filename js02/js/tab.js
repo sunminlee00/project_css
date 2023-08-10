d22c16f6395511da8fe486552bd8f74075c0dd28
lee <lee@naver.com>
2023-08-10 21:11:44 +0900

    finish

--- js02/js/tab.js
+++ js02/js/tab.js
@@ -13,10 +13,28 @@ const init=function(){
 init();
 
 mytabs.forEach(item=>{
-    item.firstChild.addEventListener('focus', function(e){
+    item.firstElementChild.addEventListener('focus', function(e){
        e.preventDefault();
-       for(let i=0; i <  mytabl)     
-   
+      
+       for(let i=0; i<mytabs.length; i++)
+       {
+        mytabs[i].firstElementChild.classList.remove('bgpanel');
+       }
+
+       for(let i=0; i<tabpanels.length; i++) 
+       {  
+         tabpanels[i].classList.remove('visit');
+       }     
+        e.target.classList.add('bgpanel');
+        let data=e.target.parentElement.getAttribute('aria-controls');
+
+        console.log(data);    
+        
+        if(data==null)
+            data='tabpanel-1';
+        document.getElementById(data).classList.add('visit');  
+
+
 
     });
 
